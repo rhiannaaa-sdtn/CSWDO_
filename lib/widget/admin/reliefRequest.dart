06af71fa6ceb75b3c77166375e2b3530f43998e7
Eggy <ric.eduard.malitig@gmail.com>
2024-10-24 02:05:47 +0800

    scrollview error

--- lib/widget/admin/reliefRequest.dart
+++ lib/widget/admin/reliefRequest.dart
@@ -2,13 +2,6 @@ import 'package:cwsdo/views/admin/side_bar.dart';
 import 'package:cwsdo/widget/custom/numberInput.dart';
 import 'package:cwsdo/widget/custom/table.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-// import 'package:cwsdo/widget/admin/addBeneficiary.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:cwsdo/widget/admin/totaltally.dart';
-// import 'package:cwsdo/widget/admin/reliefRequest.dart';
 
 class ReliefrequestMain extends StatefulWidget {
   const ReliefrequestMain({super.key});
@@ -30,9 +23,9 @@ class Reliefrequest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:
-          const EdgeInsets.only(top: 10.0, left: 50.0, right: 50, bottom: 20),
-      child: Column(
+      padding: const EdgeInsets.only(top: 10.0, left: 50.0, right: 50, bottom: 20),
+      child: SingleChildScrollView( // Added to handle overflow
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -43,35 +36,31 @@ class Reliefrequest extends StatelessWidget {
             Column(
               children: [
                 Container(
-                  color: Color.fromARGB(255, 45, 127, 226),
+                  color: const Color.fromARGB(255, 45, 127, 226),
                   height: 30,
                   width: double.infinity,
-                  // child: Tex t('data'),
                 ),
                 Container(
                   color: Colors.white,
                   width: double.infinity,
                   child: Padding(
-                    padding: const EdgeInsets.only(bottom: 18,left: 18,right: 18),
+                    padding: const EdgeInsets.only(bottom: 18, left: 18, right: 18),
                     child: Column(
                       children: [
-                        Container(
-                          child: const Row(children: [
-                            Text(
-                              'Show',
-                            ),
-                            NumberInputWidget()
-                          ]),
-                        ),
+                        Row(children: [
+                          const Text('Show'),
+                          const NumberInputWidget(),
+                        ]),
                         const TableSample(),
                       ],
                     ),
                   ),
-                  // child: Tex t('data'),
-                )
+                ),
               ],
             ),
-          ]),
+          ],
+        ),
+      ),
     );
   }
 }
