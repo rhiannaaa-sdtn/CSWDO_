7e43e927eb25a235dc13e64375710b7044910041
Eggy <ric.eduard.malitig@gmail.com>
2024-11-28 21:26:28 +0800

    asd

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:cwsdo/widget/custom/custom_widget.dart';
 import 'package:cwsdo/widget/navigation_bar/navigation_bar.dart';
 import 'package:cwsdo/constatns/navitem.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatefulWidget {
+  
   const HomePage({super.key});
 
   @override
@@ -13,17 +15,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+    int _currentIndex = 0; 
   final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
+// Now Navbar is valid as appBar
+  body: LayoutBuilder(
+    builder: (context, constraints) {
+      return ListView(
         controller: _scrollController,
         scrollDirection: Axis.vertical,
         children: [
-          // NAVBAR
-          Container(
+               Container(
             height: MediaQuery.of(context).size.height * .1,
             width: double.maxFinite,
             color: const Color(0xff08436d),
@@ -61,32 +66,84 @@ class _HomePageState extends State<HomePage> {
           ),
           // MAIN CONTENT
           Container(
-            height: MediaQuery.of(context).size.height * .9,
+            height: constraints.maxHeight * .9,
             width: double.maxFinite,
             decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("images/bg1.png"),
-                fit: BoxFit.cover,
-              ),
+              // image: DecorationImage(
+              //   image: AssetImage("images/bg1.png"),
+              //   fit: BoxFit.cover,
+              // ),
             ),
-            child: const Column(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                CustomWidg(txt: 'Community Needs Assessment', fsize: 50),
-                CustomWidg(txt: 'Management System', fsize: 50),
-                SizedBox(height: 20),
-                CustomWidg(txt: 'City of San Pablo', fsize: 30),
-                CustomWidg(
-                    txt: 'City Social Welfare and Development Office',
-                    fsize: 30),
+                Stack(
+                  alignment: Alignment.bottomCenter, // Position the indicator at the bottom center
+                  children: [
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        enlargeCenterPage: true,
+                        aspectRatio: constraints.maxWidth > 600 ? 2.6 : 2,
+                        viewportFraction: 1.0,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            _currentIndex = index; // Update the index when the page changes
+                          });
+                        },
+                      ),
+                      items: [
+                        buildCarouselSlide("images/carousel1.jpg"),
+                        buildCarouselSlide("images/carousel2.jpg"),
+                        buildCarouselSlide("images/carousel3.jpg"),
+                      ],
+                    ),
+                    // Page indicator positioned inside the carousel
+                    Positioned(
+                      bottom: 20, // Adjust the position as needed
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(3, (index) {
+                          return AnimatedContainer(
+                            duration: const Duration(milliseconds: 300),
+                            margin: const EdgeInsets.symmetric(horizontal: 4),
+                            height: 8,
+                            width: 8,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: _currentIndex == index
+                                  ? Colors.blue
+                                  : Colors.grey,
+                            ),
+                          );
+                        }),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
           const About(),
           const Footer(),
         ],
+      );
+    },
+  ),
+);
+
+  }
+}
+
+
+  Widget buildCarouselSlide(String imagePath) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      child: Image.asset(
+        imagePath,
+        fit: BoxFit.cover,
+        width: double.infinity,
       ),
     );
   }
-}
